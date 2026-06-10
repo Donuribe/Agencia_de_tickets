e02513d4d6a41ce89de1eefdacc09cefb384c583
Proyecto <proyecto@sistema.com>
2026-06-09 22:02:32 -0500

    fix: add static file router for PHP built-in server

---
+++ router.php
@@ -0,0 +1,11 @@
+<?php
+
+$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
+// Servir archivos estáticos directamente si existen
+if ($uri !== '/' && file_exists(__DIR__ . '/public' . $uri)) {
+    return false;
+}
+
+// Todo lo demás va a Laravel
+require_once __DIR__ . '/public/index.php';
